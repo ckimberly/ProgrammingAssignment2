0b531bbaba844c2018611b2c2a1918977338c8dd
ckimberly <kimberly062000@hotmail.com>
2014-06-21 23:23:44 -0700

    Update cachematrix.R
    Caching the Inverse of a Matrix

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,63 @@
-## Put comments here that give an overall description of what your
-## functions do
+## A pair of functions that cache the inverse a matrix.
+## First function is to create a matrix that can cache its inverse.
+## Second function is to retrieve the inverse matrix.
 
-## Write a short comment describing this function
+## This function creates a special "matrix" that can
+## cache its inverse.
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  m <- NULL
+  ## create a vector for inverse matrix
+  
+  set <- function(y) {           
+    x <<- y   
+    m <<- NULL 
+  }
+  ## set value of the matrix
+  
+  get <- function() x    
+  ## get the value of the matrix and return the matrix
+  
+  setinv <- function(solve) m <<- solve
+  ## set the value of the inverse matrix
+  
+  getinv <- function() m
+  ## return the value of the inverse matrix
+  
+  list(set = set, get = get,
+       setinv = setinv,
+       getinv = getinv)
+  ##return the created vector of functions
 }
 
 
-## Write a short comment describing this function
+##This function computes the inverse of the special "matrix" 
+##returned by makeCacheMatrix above. If the inverse has already
+##been calculated (and the matrix has not changed), then the 
+##cachesolve should retrieve the inverse from the cache.
+
+## The function returns a inverse matrix
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+        ##return a matrix that is the inverse of 'x'
+  m <- x$getinv()        
+  ## get the inverse matrix out of cache
+  
+  if(!is.null(m)) {                       
+    message("getting cached data")
+    return(m)                       
+  }
+  ## return to the inverse matrix if it is set
+  
+  data <- x$get() 
+  ## get the matrix
+  
+  m <- solve(data, ...)
+  ## stored in cache
+  
+  x$setinv(m)                             
+  ## set the inverse matrix
+  
+  m                                       
+  ## Return the matrix       
 }
